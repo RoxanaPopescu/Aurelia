eb78f3c5eb0c72afd6c85b7f3e15e2c6c5a9009e
Thomas Darling <mail@thda.dk>
2019-05-10 12:47:49 +0200

    data-table - work in progress

--- frontend/src/shared/framework/components/data-table/data-table-row.ts
+++ frontend/src/shared/framework/components/data-table/data-table-row.ts
@@ -1,9 +1,12 @@
-import { autoinject } from "aurelia-framework";
+import { autoinject, containerless, bindable } from "aurelia-framework";
 
 /**
  * Represents a row in a data table.
  */
+@containerless
 @autoinject
 export class DataTableRowCustomElement
 {
+    @bindable
+    public href: string | undefined;
 }
