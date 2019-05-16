cc90a60454636b4d2cd19735eac3b95d94212582
Thomas Darling <mail@thda.dk>
2019-05-16 15:10:31 +0200

    Added support for data-table-details

--- frontend/src/shared/framework/components/data-table/data-table-details.ts
+++ frontend/src/shared/framework/components/data-table/data-table-details.ts
@@ -1,9 +1,15 @@
-import { autoinject } from "aurelia-framework";
+import { autoinject, bindable } from "aurelia-framework";
+import { AccentColor } from "resources/styles";
 
 /**
- * Represents a details row in a data table, whic spans all columns.
+ * Represents a details row in a data table, which spans all columns.
  */
 @autoinject
 export class DataTableDetailsCustomElement
 {
+    /**
+     * The accent to apply to the row, or undefined to apply no accent.
+     */
+    @bindable
+    public accent: AccentColor | undefined;
 }
