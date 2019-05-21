6e9f99e2c5e3b317eaf3d570e7ad804557098a6c
Thomas Darling <mail@thda.dk>
2019-05-21 15:12:34 +0200

    Improved data-table-pager

--- frontend/src/shared/framework/components/data-table/data-table-pager.ts
+++ frontend/src/shared/framework/components/data-table/data-table-pager.ts
@@ -7,22 +7,80 @@ import { autoinject, bindable } from "aurelia-framework";
 export class DataTablePagerCustomElement
 {
     /**
+     * The value of the page number input.
+     */
+    protected pageInputValue: string;
+
+    /**
      * The current page number, starting from 1.
      */
     @bindable({ defaultValue: 1 })
     public page: number;
 
     /**
-     * The total number of pages, or undefined to apply no limits.
+     * The total number of pages, or undefined to apply no upper limit.
      */
-    @bindable({ defaultValue: 1 })
+    @bindable
     public pages: number | undefined;
 
     /**
      * The max number of items to show on a page, or undefined to disable this option.
      */
-    @bindable({ defaultValue: 20 })
+    @bindable
     public pageSize: number | undefined;
+
+    /**
+     * Called by the framework when the component is binding.
+     */
+    protected bind(): void
+    {
+        this.pageChanged();
+    }
+
+    /**
+     * Called by the framework when the `page` property changes.
+     * Ensures the number shown in the input matches the current page number.
+     */
+    protected pageChanged(): void
+    {
+        this.pageInputValue = this.page.toString();
+    }
+
+    /**
+     * Called when the page input receives focus.
+     * Selects all content in the input.
+     * @param event The focus event.
+     */
+    protected onPageInputFocus(event: FocusEvent): void
+    {
+        (event.target! as HTMLInputElement).select();
+    }
+
+    /**
+     * Called when the page input loses focus.
+     * Ensures the number shown in the input matches the current page number.
+     */
+    protected onPageInputBlur(): void
+    {
+        this.pageInputValue = this.page.toString();
+    }
+
+    /**
+     * Called when a key is pressed while the input is focused.
+     * If the key being pressed is `Enter`, commits the entered value.
+     * @param event The keyboard event.
+     */
+    protected onPageInputKeydown(event: KeyboardEvent): boolean
+    {
+        if (event.key === "Enter")
+        {
+            const page = Number.parseInt(this.pageInputValue || this.page.toString());
+            this.page = Math.max(1, Math.min(this.pages || page, page));
+            this.pageInputValue = this.page.toString();
+        }
+
+        return true;
+    }
 
     /**
      * Called when one of the navigation buttons are pressed.
@@ -32,15 +90,5 @@ export class DataTablePagerCustomElement
     protected onNavClick(offset: number): void
     {
         this.page += offset;
-    }
-
-    /**
-     * Called when an input element receives focus.
-     * Selects all content in the input.
-     * @param inputElement The input element that received focus.
-     */
-    protected onInputFocus(inputElement: HTMLInputElement): void
-    {
-        inputElement.select();
     }
 }
