def739aa69e6ab37a92b745ef9870474685535eb
Thomas Darling <mail@thda.dk>
2019-06-19 23:53:05 +0200

    Removed JSON revivers, as we prefer manual parsing

--- frontend/src/shared/utilities/index.ts
+++ frontend/src/shared/utilities/index.ts
@@ -2,7 +2,6 @@ export * from "./base-64/base-64";
 export * from "./decorators/once";
 export * from "./event-manager/event-manager";
 export * from "./id/id";
-export * from "./json-reviver/json-reviver";
 export * from "./promise-controller/promise-controller";
 export * from "./timing/delay";
 export * from "./math/math";
