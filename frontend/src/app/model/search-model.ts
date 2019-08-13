d0f2ba73519d1ee846b0c335dadb8ac148e9087c
Thomas Darling <mail@thda.dk>
2019-08-13 15:08:35 +0200

    Fixed circular reference handling in SearchModel

--- frontend/src/app/model/search-model.ts
+++ frontend/src/app/model/search-model.ts
@@ -18,21 +18,36 @@ export class SearchModel
     /**
      * Determines whether the search model contains the specified text.
      * @param text The text to search for.
+     * @param caseSensitive True to do a case sensitive search, otherwise false.
      * @returns True if the model contains the specified text, otherwise false.
      */
-    public contains(text?: string): boolean
+    public contains(text?: string, caseSensitive = false): boolean
     {
+        // An empty query is always a match.
         if (!text)
         {
             return true;
         }
 
+        // Get a JSON representation of the entity, ignoring circular references.
+
+        const objects = new Set<any>();
+
+        objects.add(this);
+        objects.add(this._entity);
+
         if (this._json == null)
         {
-            this._json = JSON.stringify(this._entity, (key, value) => value === this ? "" : value, 1).toLowerCase();
+            this._json = JSON.stringify(this._entity, (key, value) =>
+            {
+                return key && objects.has(value) ? "" : value;
+            },
+            1).toLowerCase();
         }
 
-        const q = text.toLowerCase();
+        // Search the JSON representation of the object.
+
+        const q = caseSensitive ? text : text.toLowerCase();
 
         return new RegExp(`^\\s*"[^"]+":.*${q}.*$|^\\s*"[^"]*${q}[^"]*",?$`, "m").test(this._json);
     }
