e7db32dae66b96259ab592da992949928433d4e4
Thomas Darling <mail@thda.dk>
2020-02-03 22:43:56 +0800

    Improved duration value converter

--- frontend/src/shared/localization/converters/duration/duration.ts
+++ frontend/src/shared/localization/converters/duration/duration.ts
@@ -1,5 +1,7 @@
 import { autoinject } from "aurelia-framework";
 import { Duration } from "luxon";
+import { TemplateStringParser, TemplateString } from "shared/infrastructure";
+import durationFormatStrings from "./resources/strings/duration-format.json";
 
 /**
  * Represents a value converter that formats a duration as a localized duration string.
@@ -8,6 +10,20 @@ import { Duration } from "luxon";
 @autoinject
 export class DurationValueConverter
 {
+    /**
+     * Creates a new instance of the type.
+     * @param templateStringParser The `TemplateStringParser` instance.
+     */
+    public constructor(templateStringParser: TemplateStringParser)
+    {
+        for (const key of Object.keys(durationFormatStrings))
+        {
+            this._formats.set(key, templateStringParser.parse(durationFormatStrings[key]));
+        }
+    }
+
+    private readonly _formats = new Map<string, TemplateString>();
+
     /**
      * Converts the value for use in the view,
      * formatting the specified value as a localized duration string.
@@ -21,6 +37,70 @@ export class DurationValueConverter
             return value;
         }
 
-        return value.toFormat("h:mm");
+        let parts: any;
+
+        parts = value.shiftTo("years", "months", "days", "hours", "minutes", "seconds").toObject();
+
+        if (parts.years && parts.months)
+        {
+            return this._formats.get("years,months")?.evaluate(parts);
+        }
+
+        if (parts.years)
+        {
+            return this._formats.get("years")?.evaluate(parts);
+        }
+
+        if (parts.months && parts.days)
+        {
+            return this._formats.get("months,days")?.evaluate(parts);
+        }
+
+        if (parts.months)
+        {
+            return this._formats.get("months")?.evaluate(parts);
+        }
+
+        if (parts.days)
+        {
+            return this._formats.get("days")?.evaluate(parts);
+        }
+
+        if (parts.days && parts.hours)
+        {
+            return this._formats.get("days,hours")?.evaluate(parts);
+        }
+
+        if (parts.days)
+        {
+            return this._formats.get("days")?.evaluate(parts);
+        }
+
+        if (parts.hours && parts.minutes)
+        {
+            return this._formats.get("hours,minutes")?.evaluate(parts);
+        }
+
+        if (parts.hours)
+        {
+            return this._formats.get("hours")?.evaluate(parts);
+        }
+
+        if (parts.minutes && parts.seconds)
+        {
+            return this._formats.get("minutes,seconds")?.evaluate(parts);
+        }
+
+        if (parts.minutes)
+        {
+            return this._formats.get("minutes")?.evaluate(parts);
+        }
+
+        if (parts.seconds)
+        {
+            return this._formats.get("seconds")?.evaluate(parts);
+        }
+
+        return this._formats.get("seconds")?.evaluate({ seconds: 0 });
     }
 }
