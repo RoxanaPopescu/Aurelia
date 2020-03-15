3c00525e4468516daeee193657f0dae6dd7cee1e
Thomas Darling <mail@thda.dk>
2020-03-16 00:30:43 +0100

    #TECH-2512: Work in progress

--- frontend/src/resources/stubs/responses/route-planning-settings.ts
+++ frontend/src/resources/stubs/responses/route-planning-settings.ts
@@ -1,224 +1,261 @@
 export default
+{
+    "POST /api/v1/routeplanning/rulesets/details":
     {
-
-        "POST /api/v1/routeplanning/rulesets/details":
+        data:
         {
-            data:
+            "id": "960a7f5a-c4aa-4eef-9e73-c057e913f8f2",
+            "name": "Settings name 5",
+            "slug": "settings-name-5",
+            "restrictions":
             {
-                metadata:
+                "ferriesAllowed": false,
+                "privateRoadsAllowed": true,
+                "highwaysAllowed": true,
+                "uturnStrategy": 1,
+                "curbApproachStrategy": 2,
+                "oneRoutePlanPerStartLocation": false,
+                "timeWindowAdjustmentDelivery":
                 {
-                    created: "2020-03-05T08:32+02:00",
-                    createdBy: { id: "", username: "tda@mover.dk", email: "tda@mover.dk", firstName: "Thomas", lastName: "Darling" },
-                    lastModified: "2020-03-05T09:52+02:00",
-                    lastModifiedBy: { id: "", username: "tda@mover.dk", email: "tda@mover.dk", firstName: "Thomas", lastName: "Darling" }
+                    "start": 300,
+                    "end": 600
                 },
-                id: "rule-set-1-id",
-                name: "Rule set 1",
-                slug: "rule-set-1-slug",
-                "restrictions":
-                {
-                    "ferriesAllowed": true,
-                    "privateRoadsAllowed": true,
-                    "highwaysAllowed": true,
-                    "uturnStrategy": "allowed",
-                    "curbApproachStrategy": "either-side-of-vehicle",
-                    "timeWindowAdjustment":
-                    {
-                        "start": 12,
-                        "end": 12
-                    },
-                    "oneRoutePlanPerStartLocation": true,
-                    "maxCalculationTime": 60
-                },
-                "routeCreation":
-                {
-                    "routeTags": [],
-                    "manualApproval": false,
-                    "routeNameTemplate": ""
-                },
-                "vehicleGroups":
+                "maxCalculationTime": 3600
+            },
+            "routeCreation":
+            {
+                "routeTags":
                 [
-                    {
-                        "name": "",
-                        "id": "",
-                        "cost":
-                        {
-                            "newRoute": 123,
-                            "waitingTime": 12,
-                            "drivingTime": 12,
-                            "taskTime": 12,
-                            "distance": 12
-                        },
-                        "vehicleType": "",
-                        "limits":
-                        {
-                            "volume": 12,
-                            "weight": 1239,
-                            "colliCount": 1231,
-                            "stopCount": 56,
-                            "time": 1231,
-                            "distance": 371
-                        },
-                        "orderTagsAllRequired": [""],
-                        "orderTagsOneRequired": [""],
-                        "startLocation":
-                        {
-                            "location":
-                            {
-                                "address":
-                                {
-                                    "primary": "",
-                                    "secondary": ""
-                                },
-                                "position":
-                                {
-                                    "latitude": 12.13,
-                                    "longitude": 55.213
-                                }
-                            },
-                            "taskTime": 12.1
-                        },
-                        "endLocation":
-                        {
-                            "location":
-                            {
-                                "address":
-                                {
-                                    "primary": "",
-                                    "secondary": ""
-                                },
-                                "position":
-                                {
-                                    "latitude": 12.13,
-                                    "longitude": 55.213
-                                }
-                            },
-                            "taskTime": 12.1
-                        },
-                        "routeTags": [""]
-                    }
+                    "sometag",
+                    "anothertag",
+                    "a"
                 ],
-                "departureTimes":
-                [
+                "manualApproval": false,
+                "routeNameTemplate": "IK-{AreaRouteNumber}-{Weekday}-{RouteplanRouteNumber}-{StartTime}"
+            },
+            "vehicleGroups":
+            [
+                {
+                    "name": "Electric Van",
+                    "id": "fe6c46c9-7277-4890-92b2-e009821de944",
+                    "cost":
                     {
-                        "startLocation":
+                        "newRoute": 120,
+                        "waitingTime": 8,
+                        "drivingTime": 5,
+                        "taskTime": 5,
+                        "distance": 2
+                    },
+                    "vehicleTypeId": "2321cbd7-5bed-4035-a827-2bfea31bb8e8",
+                    "limits":
+                    {
+                        "volume": 100,
+                        "weight": 900,
+                        "colliCount": 1000,
+                        "stopCount": 30,
+                        "time": 43200,
+                        "distance": 300
+                    },
+                    "orderTagsAllRequired":
+                    [
+                        "1-man"
+                    ],
+                    "orderTagsOneRequired":
+                    [
+                        "electric",
+                        "eco"
+                    ],
+                    "endLocation":
+                    {
+                        "location":
                         {
                             "address":
                             {
-                                "primary": "",
-                                "secondary": ""
+                                "primary": "Mårkærvej 15",
+                                "secondary": "2630 Taastrup"
                             },
                             "position":
-                            {
-                                "latitude": 12.13,
-                                "longitude": 55.213
-                            }
+                            [
+                                10.2948912,
+                                55.7815602
+                            ]
                         },
-                        "scenarios":
-                        [
-                            {
-                                "name": "",
-                                "criteria":
-                                {
-                                    "weekdays": [1, 2],
-                                    "datePeriod":
-                                    {
-                                        "from": "2020-09-05T23:32:34+02:00",
-                                        "to": "2021-10-05T00:32:34+02:00"
-                                    }
-                                },
-                                "gates":
-                                [
-                                    {
-                                        "name": "Gate B",
-                                        "slots":
-                                        [
-                                            {
-                                                "arrivalTime": "2021-10-05T00:32:34+02:00",
-                                                "lastDepartureTime": "2021-10-05T08:32:34+02:00",
-                                                "timeBetweenDepartures": "00:20:00",
-                                                "vehicleGroupId": ""
-                                            }
-                                        ]
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ],
-                "taskTimes":
+                        "time": 600
+                    },
+                    "routeTags":
+                    [
+                        "electric"
+                    ]
+                }
+            ],
+            "departureTimes":
+            [
                 {
-                    "base":
+                    "startLocation":
                     {
-                        "initial": 12.132,
-                        "perFloor": 12.239,
-                        "perRound": 12.213
+                        "address":
+                        {
+                            "primary": "Mårkærvej 15",
+                            "secondary": "2630 Taastrup"
+                        },
+                        "position":
+                        [
+                            10.2948912,
+                            55.7815602
+                        ]
                     },
-                    "roundDefinition":
+                    "scenarios":
+                    [
+                        {
+                            "gates":
+                            [
+                                {
+                                    "name": "Gate B",
+                                    "slots":
+                                    [
+                                        {
+                                            "earliestArrivalTime": 23700,
+                                            "latestDepartureTime": 30120,
+                                            "vehicleGroup": "3462b6c3-1b44-4015-b971-98070d8f216c",
+                                            "timeBetweenDepartures": 1320
+                                        }
+                                    ]
+                                }
+                            ],
+                            "name": "Weekdays - no monday",
+                            "criteria":
+                            {
+                                "weekdays":
+                                [
+                                    2,
+                                    3,
+                                    4,
+                                    5
+                                ],
+                                "datePeriod":
+                                {
+                                    "from": "2021-01-19T23:00:00+01:00",
+                                    "to": "2021-12-19T23:00:00+01:00"
+                                }
+                            }
+                        }
+                    ]
+                }
+            ],
+            "taskTimes":
+            {
+                "base":
+                {
+                    "initial": 300,
+                    "perFloor": 30,
+                    "perRound": 30
+                },
+                "roundDefinition":
+                {
+                    "colliPerRound": 5,
+                    "weightPerRound": 30,
+                    "volumePerRound": 0.7
+                },
+                "scenarios":
+                [
                     {
-                        "colliPerRound": 12,
-                        "weightPerRound": 12,
-                        "volumePerRound": 13
+                        "criteria":
+                        {
+                            "orderTagsAllRequired":
+                            [
+                                "carryin",
+                                "b"
+                            ],
+                            "orderTagsOneRequired":
+                            [
+                                "c",
+                                "d"
+                            ]
+                        },
+                        "time":
+                        {
+                            "initial": 300,
+                            "perFloor": 30,
+                            "perRound": 30
+                        }
+                    }
+                ]
+            },
+            "specialAreas":
+            [
+                {
+                    "polygon":
+                    {
+                        "type": "Polygon",
+                        "coordinates":
+                        [
+                            [
+                                [
+                                    12.502155,
+                                    55.711982
+                                ],
+                                [
+                                    12.542227,
+                                    55.713366
+                                ],
+                                [
+                                    12.491405,
+                                    55.70604
+                                ],
+                                [
+                                    12.502155,
+                                    55.711982
+                                ]
+                            ]
+                        ]
                     },
+                    "name": "Jylland",
+                    "color": 2,
                     "scenarios":
                     [
                         {
                             "criteria":
                             {
-                                "orderTagsAllRequired": [""],
-                                "orderTagsOneRequired": [""]
+                                "weekdays":
+                                [
+                                    6,
+                                    1
+                                ],
+                                "datePeriod":
+                                {
+                                    "from": "2021-01-19T23:00:00+01:00",
+                                    "to": "2021-12-19T23:00:00+01:00"
+                                },
+                                "orderTagsAllRequired": [],
+                                "orderTagsOneRequired":
+                                [
+                                    "b"
+                                ]
                             },
-                            "additionalTime":
-                            {
-                                "initial": 12.132,
-                                "perFloor": 12.239,
-                                "perRound": 12.213
-                            }
+                            "taskTimeChange": 300,
+                            "drivingTimeChangeFactor": -0.12,
+                            "isAreaBlocked": false
                         }
                     ]
-                },
-                "specialAreas":
-                [
-                    {
-                        "name": "Some special area",
-                        "color": 1,
-                        "polygon": [],
-                        "scenarios":
-                        [
-                            {
-                                "criteria":
-                                {
-                                    "weekdays": [1, 2],
-                                    "datePeriod":
-                                    {
-                                        "from": "2020-09-05T23:32:34+02:00",
-                                        "to": "2021-10-05T00:32:34+02:00"
-                                    },
-                                    "orderTagsAllRequired": ["tag-1"],
-                                    "orderTagsOneRequired": ["tag-2"]
-                                },
-                                "taskTimeChange": 12,
-                                "drivingTimeChangeFactor": 0.12,
-                                "isBlocked": false
-                            }
-                        ]
-                    }
-                ]
-            }
-        },
-
-        "POST /api/v1/routeplanning/settingsv2/create":
-        {
-            status: 201
-        },
-
-        "POST /api/v1/routeplanning/settingsv2/update":
-        {
-            status: 201
-        },
-
-        "POST /api/v1/routeplanning/settingsv2/delete":
-        {
-            status: 201
+                }
+            ],
+            "lastModifiedBy": "04eb462a-dc64-4a5e-b904-3c600cf8c02e",
+            "lastModified": "2020-03-12T08:33:39.8344131"
         }
-    };
+    },
+
+    "POST /api/v1/routeplanning/settingsv2/create":
+    {
+        status: 201
+    },
+
+    "POST /api/v1/routeplanning/settingsv2/update":
+    {
+        status: 201
+    },
+
+    "POST /api/v1/routeplanning/settingsv2/delete":
+    {
+        status: 201
+    }
+};
