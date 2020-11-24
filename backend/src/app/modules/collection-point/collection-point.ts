036780fa5ef7dfa22ed0bc5bef87734ab3b470c1
August Ras Jørgensen <arj@usemover.com>
2020-11-24 16:38:48 +0100

    #TECH-4539 Added remaining events to collection point

--- backend/src/app/modules/collection-point/collection-point.ts
+++ backend/src/app/modules/collection-point/collection-point.ts
@@ -16,21 +16,124 @@ export class CollectionPointModule extends AppModule
         {
             const loginResult = await this.validateDriverLogin(context);
 
-            console.log(
-                loginResult.driver.id
-            );
-
             const result = await this.apiClient.post("collection-point/details",
             {
                 body:
                 {
                     id: context.params.id,
-                    outfitId: loginResult.outfitId,
-                    ownerId: loginResult.outfitId
+                    outfitId: loginResult.outfitId
                 }
             });
 
-            // FIXME: Remove owner id
+            context.response.body = result.data;
+            context.response.status = 200;
+        });
+
+        /**
+         * Saves the colected order event
+         * @returns 200 when the event is received
+         */
+        this.router.post("/v2/collection-point/order/collected", async context =>
+        {
+            const loginResult = await this.validateDriverLogin(context);
+
+            const result = await this.apiClient.post("collection-point/order/collected",
+            {
+                body:
+                {
+                    ...context.request.body,
+                    outfitId: loginResult.outfitId,
+                    actionById: this.stringToGuid(loginResult.driver.id)
+                }
+            });
+
+            context.response.body = result.data;
+            context.response.status = 200;
+        });
+
+        /**
+         * Saves the missing order event
+         * @returns 200 when the event is received
+         */
+        this.router.post("/v2/collection-point/order/missing", async context =>
+        {
+            const loginResult = await this.validateDriverLogin(context);
+
+            const result = await this.apiClient.post("collection-point/order/missing",
+            {
+                body:
+                {
+                    ...context.request.body,
+                    outfitId: loginResult.outfitId,
+                    actionById: this.stringToGuid(loginResult.driver.id)
+                }
+            });
+
+            context.response.body = result.data;
+            context.response.status = 200;
+        });
+
+        /**
+         * Saves the damaged order event
+         * @returns 200 when the event is received
+         */
+        this.router.post("/v2/collection-point/order/damaged", async context =>
+        {
+            const loginResult = await this.validateDriverLogin(context);
+
+            const result = await this.apiClient.post("collection-point/order/damaged",
+            {
+                body:
+                {
+                    ...context.request.body,
+                    outfitId: loginResult.outfitId,
+                    actionById: this.stringToGuid(loginResult.driver.id)
+                }
+            });
+
+            context.response.body = result.data;
+            context.response.status = 200;
+        });
+
+        /**
+         * Saves the rejected order event
+         * @returns 200 when the event is received
+         */
+        this.router.post("/v2/collection-point/order/rejected", async context =>
+        {
+            const loginResult = await this.validateDriverLogin(context);
+
+            const result = await this.apiClient.post("collection-point/order/rejected",
+            {
+                body:
+                {
+                    ...context.request.body,
+                    outfitId: loginResult.outfitId,
+                    actionById: this.stringToGuid(loginResult.driver.id)
+                }
+            });
+
+            context.response.body = result.data;
+            context.response.status = 200;
+        });
+
+        /**
+         * Saves the not collected order event
+         * @returns 200 when the event is received
+         */
+        this.router.post("/v2/collection-point/order/not-collected", async context =>
+        {
+            const loginResult = await this.validateDriverLogin(context);
+
+            const result = await this.apiClient.post("collection-point/order/NotCollected",
+            {
+                body:
+                {
+                    ...context.request.body,
+                    outfitId: loginResult.outfitId,
+                    actionById: this.stringToGuid(loginResult.driver.id)
+                }
+            });
 
             context.response.body = result.data;
             context.response.status = 200;
@@ -53,5 +156,18 @@ export class CollectionPointModule extends AppModule
         });
 
         return result.data;
+    }
+
+    /**
+     * Returns the driver id as a guid
+     * @param id the string of the driver
+     * @returns The id of the driver with leading 0's as a GUID
+     */
+    private stringToGuid(id: string): string
+    {
+        const totalLength = 12;
+        const paddingLength = totalLength - id.length;
+
+        return `00000000-0000-0000-0000-${"0".repeat(paddingLength)}${id}`;
     }
 }
