350d6b2f4543bd222db8e92fb450485af7220893
Thomas Darling <mail@thda.dk>
2021-02-19 11:36:41 +0100

    Include owner ID in order events request

--- backend/src/app/modules/orders/modules/events/order-events.ts
+++ backend/src/app/modules/orders/modules/events/order-events.ts
@@ -9,17 +9,19 @@ export class OrderEventsModule extends AppModule
     {
         /**
          * Gets the order events for the specified order.
-         * @param context.params.orderId The ID of the order for which to get events.
+         * @param context.request.body.consignorId The ID of the consignor owner of the order.
+         * @param context.request.body.orderId The ID of the order for which to get events.
          * @returns The order events for the specified order.
          */
-        this.router.get("/v2/orders/events/:orderId", async context =>
+        this.router.post("/v2/orders/events", async context =>
         {
             // Fetch the order events.
             const result = await this.apiClient.post("orderevent/GetEventsV2",
             {
                 body:
                 {
-                    ownerId: context.params.orderId
+                    ownerId: context.request.body.consignorId,
+                    ownerOrderId: context.request.body.orderId
                 }
             });
 
