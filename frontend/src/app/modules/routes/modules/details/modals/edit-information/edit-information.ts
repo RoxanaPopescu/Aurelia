57d84ff8df6006dc6131f103a214dd967cb4792f
August Jørgensen <arj@usemover.com>
2021-06-15 08:30:47 +0200

    linting

--- frontend/src/app/modules/routes/modules/details/modals/edit-information/edit-information.ts
+++ frontend/src/app/modules/routes/modules/details/modals/edit-information/edit-information.ts
@@ -49,7 +49,7 @@ export class EditInformationPanel
     /**
      * The available vehicle types.
      */
-     public vehicleTypes: VehicleType[];
+    public vehicleTypes: VehicleType[];
 
     /**
      * Called by the framework when the modal is activated.
