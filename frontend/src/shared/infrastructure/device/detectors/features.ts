1bb9a4a58ba553a95636e485af4d72224fd6ffae
Thomas Darling <mail@thda.dk>
2021-10-26 22:43:38 +0200

    Prevent scrollbars from appearing during modal animation

--- frontend/src/shared/infrastructure/device/detectors/features.ts
+++ frontend/src/shared/infrastructure/device/detectors/features.ts
@@ -1,4 +1,12 @@
 /**
+ * Returns true if the user agent indicates it prefers a dark color scheme, otherwise false.
+ */
+export function darkMode(): boolean
+{
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+}
+
+/**
  * Returns true if `sessionStorage` is supported and writable, otherwise false.
  */
 export function sessionStorage(): boolean
@@ -37,9 +45,22 @@ export function localStorage(): boolean
 }
 
 /**
- * Returns true if the user agent indicates it prefers a dark color scheme, otherwise false.
+ * Returns true if scrollbars always appear as overlays, otherwise false.
  */
-export function darkMode(): boolean
+export function overlayScrollbar(): boolean
 {
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+    const element = document.createElement("div");
+    element.style.position = "fixed";
+    element.style.top = "-100px";
+    element.style.width = "100px";
+    element.style.height = "100px";
+    element.style.overflow = "scroll";
+    element.style.opacity = "0";
+    document.body.appendChild(element);
+
+    const scrollbarWidth = element.offsetWidth - element.clientWidth;
+
+    document.body.removeChild(element);
+
+    return scrollbarWidth === 0;
 }
