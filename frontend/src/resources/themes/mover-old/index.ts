3570138111447759e8276d7aab826d46909f19f2
Thomas Darling <mail@thda.dk>
2021-10-26 22:50:16 +0200

    Removed old Mover theme

--- frontend/src/resources/themes/mover-old/index.ts
+++
@@ -1,9 +0,0 @@
-import settings from "resources/settings";
-
-// Configure the Google Maps integration.
-
-// Not that the map IDs may be customized here:
-// https://console.cloud.google.com/google/maps-apis/client-styles/c539673dac46cccb
-
-settings.integrations.googleMaps.parameters.mapIds = ["d9a40ce604bf0ef8"];
-settings.integrations.googleMaps.defaults!.mapId = "d9a40ce604bf0ef8";
