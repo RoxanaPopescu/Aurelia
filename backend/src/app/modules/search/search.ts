07f83fcd4d1fbb4dc871e38cbdda270acff4794d
Thomas Darling <mail@thda.dk>
2021-10-29 17:09:39 +0200

    Minor fixes

--- backend/src/app/modules/search/search.ts
+++ backend/src/app/modules/search/search.ts
@@ -71,7 +71,7 @@ export class RoutesModule extends AppModule
                     include: { owner: true },
 
                     // HACK: This request times out if we don't limit the search to recent routes.
-                    startTimeFrom: DateTime.utc().minus({ weeks: 2 })
+                    startTimeFrom: DateTime.utc().minus({ month: 1 })
                 }
             }),
 
