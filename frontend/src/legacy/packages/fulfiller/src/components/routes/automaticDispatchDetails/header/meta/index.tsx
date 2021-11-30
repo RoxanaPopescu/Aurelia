dc0894140f5fc318760d1145bd1c9e695cc57ddb
August Ras Jørgensen <arj@mover.dk>
2021-11-30 12:36:03 +0100

    Show driver on meta page of automatic dispatch job

--- frontend/src/legacy/packages/fulfiller/src/components/routes/automaticDispatchDetails/header/meta/index.tsx
+++ frontend/src/legacy/packages/fulfiller/src/components/routes/automaticDispatchDetails/header/meta/index.tsx
@@ -49,6 +49,14 @@ export default class MetaHeaderComponent extends React.Component<Props> {
           title={Localization.sharedValue("Timeframe")}
           description={Localization.formatDateTimeRange(new DateTimeRange({ from: (route.stops[0] as RouteStop).estimates!.timeFrame.from, to: (route.stops[route.stops.length-1] as RouteStop).estimates!.timeFrame.to }))}
         />
+        <HeaderItemComponent
+          title="Id"
+          description={route.slug}
+        />
+        <HeaderItemComponent
+          title={Localization.operationsValue("Dispatch_Driver")}
+          description={(route.driver?.name.toString() ?? "") + " (" + (route.driver?.id ?? "") + ")"}
+        />
       </React.Fragment>
     );
   }
