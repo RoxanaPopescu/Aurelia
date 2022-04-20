9d7b2862ae80ba102cca490ebc0370545afbfa55
Thomas Darling <mail@thda.dk>
2022-04-20 15:07:54 +0200

    Minor fixes

--- frontend/src/app/model/order/entities/order-import-result.ts
+++ frontend/src/app/model/order/entities/order-import-result.ts
@@ -1,20 +1,20 @@
 /**
- * Represents the status of a completed import operation.
+ * Represents the status of a completed order import operation.
  */
 export type OrderImportStatus = "success" | "failure";
 
 /**
- * Represents the result of a completed import operation.
+ * Represents the result of a completed order import operation.
  */
 export type OrderImportResult = IOrderImportSuccessResult | IOrderImportFailureResult;
 
 /**
- * Represents the result of a successful import operation.
+ * Represents the result of a successful order import operation.
  */
 export interface IOrderImportSuccessResult
 {
     /**
-     * The status of the completed import operation.
+     * The status of the completed order import operation.
      */
     status: "success";
 
@@ -25,12 +25,12 @@ export interface IOrderImportSuccessResult
 }
 
 /**
- * Represents the result of a failed import operation.
+ * Represents the result of a failed order import operation.
  */
 export interface IOrderImportFailureResult
 {
     /**
-     * The status of the completed import operation.
+     * The status of the completed order import operation.
      */
     status: "failure";
 
@@ -41,18 +41,33 @@ export interface IOrderImportFailureResult
 }
 
 /**
- * Represents an error associated with a failed import operation.
+ * Represents an error associated with a failed order import operation.
  */
 export interface IOrderImportError
 {
     /**
-     * The range identifying the rows and columns related to the error, if any.
+     * The range identifying the cells related to the error, if any.
      */
     range?:
     {
+        /**
+         * The first column in the range.
+         */
         fromColumn: string,
+
+        /**
+         * The first row in the range.
+         */
         fromRow: number,
+
+        /**
+         * The last column in the range.
+         */
         toColumn: string,
+
+        /**
+         * The last column in the range.
+         */
         toRow: number
     },
 
