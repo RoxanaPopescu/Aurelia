d1d8e8d83a603aed93d4d212fa6cee5370ffec54
August Ras Jørgensen <arj@mover.dk>
2022-04-29 11:55:01 +0200

    Revert "Removed custom team code from route list. It has been generalised already."
    This reverts commit 876f30c6b9530f15200edb947a93409834e92f23.

--- backend/src/app/modules/routes/modules/list/list.ts
+++ backend/src/app/modules/routes/modules/list/list.ts
@@ -19,6 +19,35 @@ export class RoutesListModule extends AppModule
         body.fulfillerIds = [context.user?.organizationId];
         body.organizationId = context.user?.organizationId;
 
+        // TODO: Generalize this part
+        const accessAllTeams = context.user?.permissions.has("access-all-teams") ?? false;
+
+        let teams = body.teams;
+        if (teams == null)
+        {
+            if (!accessAllTeams)
+            {
+                teams = context.user?.teamIds ?? [];
+            }
+        }
+
+        if (teams == null)
+        {
+            delete body.teams;
+        }
+        else
+        {
+            const teamIds = teams.filter((t: any) => t !== "no-team");
+            const includeNoTeam: boolean | undefined = teams.some((t: any) => t === "no-team");
+
+            body.teams =
+            {
+                ids: teamIds ?? [],
+                includeNoTeam: includeNoTeam ?? true
+            };
+        }
+        // TODO: End generalize
+
         const result = await this.apiClient.post("logistics-platform/routes/v4/list",
         {
             noi: true,
