fd975733c803fd1d91727e089d549779424f9f69
Thomas Darling <mail@thda.dk>
2022-10-09 02:48:51 +0700

    Add error-indicator

---
+++ frontend/src/shared/framework/components/indicators/error-indicator/error-indicator.ts
@@ -0,0 +1,9 @@
+import { autoinject } from "aurelia-framework";
+
+/**
+ * Represents an indicator to be shown when a query returns an error.
+ */
+@autoinject
+export class ErrorIndicatorCustomElement
+{
+}
